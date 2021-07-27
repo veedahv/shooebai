bf9cd80bc0434bbbf0099334853a0c29b0c53a8f
veedahv <veedahv55@gmail.com>
2021-07-27 22:14:18 +0100

    new commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <!-- <Navigation></Navigation> -->
+    <Navigation></Navigation>
     <Carousel></Carousel>
     <section>
-      <div class="container py-5 mx-auto">
+      <div class="container py-25 mx-auto">
         <div class="">
-          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            <div class="" v-for="product in products" :key="product.id">
-              <ProductCard :product="product"></ProductCard>
+          <div class="sub-heading-box text-center py-5 relative">
+            <hr class="border-0 bg-black w-full h-px inset-y-1/2 left-0 absolute">
+            <h2 class="sub-heading bg-white mx-auto py-2 w-min relative">Trending</h2>
+          </div>
+          <div class="">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              <div class="" v-for="product in products" :key="product.id">
+                <ProductCard :product="product"></ProductCard>
+              </div>
             </div>
           </div>
         </div>
@@ -18,12 +24,12 @@
 
 <script>
 import ProductCard from "../components/ProductCard";
-// import Navigation from '../components/Navigation'
+import Navigation from '../components/Navigation'
 import Carousel from "../components/Carousel";
 export default {
   components: {
     ProductCard,
-    // Navigation,
+    Navigation,
     Carousel,
   },
   data() {
