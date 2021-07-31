45ccfe1cd209fc6e18627cc4fc5a7d296469e5b9
veedahv <veedahv55@gmail.com>
2021-07-31 10:24:50 +0100

    new commit

--- components/footer.vue
+++ components/footer.vue
@@ -2,6 +2,28 @@
   <div>
     <div class="container py-5 px-4 mx-auto">
       <div>My footer</div>
+      <div class="flex flex-wrap justify-between items-center">
+        <!-- <div class="py-2 w-full md:w-2/4 lg:w-1/4"> -->
+        <div class="py-2 w-full sm:w-2/4 lg:w-1/4">
+          <nuxt-link to="/">Sho_oebai</nuxt-link>
+        </div>
+        <!-- <div class="py-2 w-full md:w-2/5 lg:w-1/5"> -->
+        <div class="py-2 w-1/2 sm:w-2/5 lg:w-1/5">
+          <nuxt-link to="/">ball</nuxt-link> |
+          <nuxt-link to="/Admin">Admin</nuxt-link> | 
+          <!-- <button @click="signOut" v-if="isLoggedIn">logout</button> -->
+          <!-- <button @click="signIn" v-else>login</button> -->
+        </div>
+        <!-- <div class="py-2 w-full md:w-2/5 lg:w-1/5"> -->
+        <div class="py-2 w-1/2 sm:w-2/5 lg:w-1/5">
+          <nuxt-link to="/">ball</nuxt-link> |
+          <nuxt-link to="/Admin">Admin</nuxt-link> | 
+          <!-- <button @click="signOut" v-if="isLoggedIn">logout</button> -->
+          <!-- <button @click="signIn" v-else>login</button> -->
+        </div>
+        <!-- <div class="py-2 w-full md:w-2/4 lg:w-1/4">3</div> -->
+        <div class="py-2 w-full sm:w-2/4 lg:w-1/4">3</div>
+      </div>
     </div>
   </div>
 </template>
